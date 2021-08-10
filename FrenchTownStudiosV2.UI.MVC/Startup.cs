950534d72b555b1ba641bf66f8f2fc0f5d760287
Jwoodworth515 <84031813+Jwoodworth515@users.noreply.github.com>
2021-08-09 22:02:17 -0500

    Identity Samples Commit

---
+++ FrenchTownStudiosV2.UI.MVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace FrenchTownStudiosV2.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
