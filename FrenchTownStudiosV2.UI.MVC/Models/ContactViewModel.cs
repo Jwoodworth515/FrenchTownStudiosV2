076156e5150d8a06631115d2af5678acd54d4660
Jwoodworth515 <84031813+Jwoodworth515@users.noreply.github.com>
2021-08-11 10:05:24 -0500

    Contact Form Addition and Test Complete

--- FrenchTownStudiosV2.UI.MVC/Models/ContactViewModel.cs
+++ FrenchTownStudiosV2.UI.MVC/Models/ContactViewModel.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace FrenchTownStudiosV2.UI.MVC.Models
 {
-    public class ConactViewModel
+    public class ContactViewModel
     {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
     }
 }
