d3cdf95c890eef7398fc3fbbb92ec398231dcd34
Jwoodworth515 <84031813+Jwoodworth515@users.noreply.github.com>
2021-08-11 10:35:56 -0500

    Add jQuery.datatables and paging

---
+++ FrenchTownStudiosV2.UI.MVC/Controllers/FiltersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FrenchTownStudiosV2.DATA.EF;
+using System.Data.Entity;
+using System.Data;
+using System.Net;
+
+namespace FrenchTownStudiosV2.UI.MVC.Controllers
+{
+    public class FiltersController : Controller
+    {
+        private FSDPRegistrationSystemEntities db = new FSDPRegistrationSystemEntities();
+        // GET: Filters
+        public ActionResult ReservationTable()
+        {
+            DbSet<Reservation> reservations = db.Reservations;
+            return View();
+        }
+    }
+}
